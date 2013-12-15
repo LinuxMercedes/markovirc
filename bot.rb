3da1bdc982d5398f647552223eaee7f5e0b9232b
LinuxMercedes <LinuxMercedes@gmail.com>
2013-12-15 00:36:12 -0600

    Fixing regexes to not be overzealous

--- bot.rb
+++ bot.rb
@@ -16,22 +16,24 @@ bot = Cinch::Bot.new do
     c.delimeter = "!"
   end
 
-  on :message, /('?sup|he[y]+|hello)[\s]*([a-z0-9_-]*)/i do |m, greeting, text|
+  on :message, /^('?sup|he[y]+|hello)[\s]*(\s[a-z0-9_-]*)/i do |m, greeting, text|
     if text != "" and text != bot.nick
       next
     end
     
     if m.user.nick == "lae"
       m.reply "Hey Musee!"
+		elsif m.user.nick == 'Aaron5367'
+			m.reply "/xe/ billy!"
     else
       m.reply "Hello #{m.user.nick}"
     end
   end
-  
-  on :message, /!([a-z]*)(.*)/i do |msg, command, args|
+
+  on :message, /^!([a-z]*)(.*)/i do |msg, command, args|
     commandHandle command, args, msg
   end
-  
+
   on :message do |msg|
     logHandle $db, msg
   end
