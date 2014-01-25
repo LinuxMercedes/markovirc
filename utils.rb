8a8a6d08a1434497d635e8b2c54c51f9a241b255
Billy Rhoades <billyarhoades@gmail.com>
2014-01-24 19:01:18 -0600

    Adding missing file from the previous commit, which contains sever.

---
+++ utils.rb
@@ -0,0 +1,10 @@
+def sever( text )
+  sentences = text.split( /[.!?]+/ )
+  sentencewords = []
+  
+  sentences.each do |sentence|
+    sentencewords << sentence.split( /[\s]+/ ).delete_if { |a| a == "" }
+  end
+  
+  return sentencewords
+end
