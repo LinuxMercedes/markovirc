de9131f2430be7bebfa1c45cea90dcdc1633a2f3
Billy Rhoades <billyarhoades@gmail.com>
2014-06-09 17:33:49 -0500

    Added missing command.

---
+++ plugins/src.rb
@@ -0,0 +1,25 @@
+require 'cinch'
+
+class Src 
+  include Cinch::Plugin
+  include Source 
+
+  match /(src|source) (.+)/, method: :execute
+
+  def execute( m, name, args )
+    args = args.strip.to_i
+    
+    #We're looking for 1 (most recent message) 2 (two messages ago) or higher.
+    if args > $bot.logs[m.channel].size or args < 1
+      return
+    end
+
+    sent = $bot.logs[m.channel][-1*args]
+    chanid = m.getFirst_i "SELECT id FROM channels WHERE name=?", m.channel
+    m.getFirst "INSERT INTO quotes (channelid, chain) VALUES (?, ?)", [chanid,sent.chainids.join(" ")]
+    qid = m.getFirst_i "SELECT currval('quotes_id_seq')"
+
+    m.reply "#{m.bot.set.quoteurl}#{qid.to_s}", true
+  end
+end
+
