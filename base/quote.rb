111cf1de86cdf189370eb0fabb5a702d2c9bfc30
Billy Rhoades <billyarhoades@gmail.com>
2014-07-01 19:37:09 -0500

    Removed some debugging and added a catch before going live.

--- base/quote.rb
+++ base/quote.rb
@@ -84,97 +84,101 @@ get '/src/' do
 end
 
 get '/src/:qid' do
-  $conn = PG.connect( :dbname => 'markovirc' )
-  # This gets our string of chain id's
-  res = exec( "SELECT chain FROM quotes WHERE id=$1", [ params[:qid] ] )
-  res = JSON.parse res
-  msg = Message.new
-  
-  out = ""
-  chains = [] # Stores a 2d-array of [ [ word color, word, text source ], ... ]
-  tids = []
-  colors = Hash.new # Stores colors in order of source id for easy zipping in
-  generator = ColorGenerator.new saturation: 0.7, lightness: 0.5, seed: params[:qid].to_i
+  if params[:qid] != params[:qid].to_i.to_s or params[:qid].to_i < 0
+    "QID Must be an integer that's greater than 0."
+  else
+    $conn = PG.connect( :dbname => 'markovirc' )
+    # This gets our string of chain id's
+    res = exec( "SELECT chain FROM quotes WHERE id=$1", [ params[:qid] ] )
+    res = JSON.parse res
+    msg = Message.new
+    
+    out = ""
+    chains = [] # Stores a 2d-array of [ [ word color, word, text source ], ... ]
+    tids = []
+    colors = Hash.new # Stores colors in order of source id for easy zipping in
+    generator = ColorGenerator.new saturation: 0.7, lightness: 0.5, seed: params[:qid].to_i
 
-  res.length.times.each do |chn|
-    chains << []
-    tid = 0
-    color = generator.create_hex
-    res[chn].each do |r|
-      chain = exec( "select wordid,textid from chains where id=$1", r )
+    res.length.times.each do |chn|
+      chains << []
+      tid = 0
+      color = generator.create_hex
+      res[chn].each do |r|
+        chain = exec( "select wordid,textid from chains where id=$1", r )
 
-      colors[chn] = color
-      tid = chain[1]
-      chains.last << [ color, chain[0], chain[1] ]
-    end
-    tids << tid 
-  end
-
-  wids = []
-  tids_index = Array.new(tids)
-  tids.uniq!
-
-  # Push what marko said out.
-  chains.map { |c| c.map { |d| wids << d[1].to_i } }
-
-  sentence = Sentence.new msg, wids
-  i = 0
-
-  flatchains = chains.flatten
-  sentence.each do |word|
-    word.suffix = "</font>"
-    word.prefix = "<font color=\"#{flatchains[i*3]}\">" 
-    i += 1
-  end
-
-  out += sentence.to_s + "<br />\n<br />\n"
-
-  srctext = Hash.new # Stores our original source text (eventually Sentences) for later
-
-  #Get our source text's chain id's
-  tids.each do |tid|
-    sent = exec "SELECT id FROM chains WHERE textid=$1", tid
-    sent.delete( sent[-1] )
-    srctext[tid] = sent.flatten
-  end
-
-  srcsent = Hash.new
-
-  #Now that we have both the source text chain id's and the quote's
-  #  we can flag text to be colored when it matches, in its entirety,
-  #  a chunk of the source text. We tag it with the color it needs.
-
-  res.length.times.each do |i|
-    tid = tids_index[i]
-    #print "TID: ", tid, "\n"
-    #print "src: ", srctext[tid], "\n", res[i], "\n\n"
-    if not srcsent.has_key? tid
-      srctext[tid] = chain_to_word srctext[tid]
-    end
-    res[i] = chain_to_word res[i]
-
-    #print "src: ", srctext[tid], "\n", "res: ", res[i], "\n"
-
-    ind = index_in srctext[tid], res[i] #Find the first occurance of this chain in this fragment & return index
-    len = res[i].length-1
-    #print "i: " + i.to_s + "\tind: " + ind.to_s + "\tlen: " + len.to_s + "\n\n"
-
-    if not srcsent.has_key? tid
-      srcsent[tid] = Sentence.new msg, ( srctext[tid] ) 
+        colors[chn] = color
+        tid = chain[1]
+        chains.last << [ color, chain[0], chain[1] ]
+      end
+      tids << tid 
     end
 
-    len.times do |j|
-      #if j+ind >= srctext[i].length 
-      #  break
-      #end
-      srcsent[tid][ind+j].prefix = "<font color=\"#{colors[i]}\">"
-      srcsent[tid][ind+j].suffix = "</font>" 
-    end  
-  end
+    wids = []
+    tids_index = Array.new(tids)
+    tids.uniq!
 
-  srcsent.values.each do |src|
-    out += "TID: " + srcsent.key(src) + "\tSRC: " + src.to_s + "<br />\n"
-  end
+    # Push what marko said out.
+    chains.map { |c| c.map { |d| wids << d[1].to_i } }
+
+    sentence = Sentence.new msg, wids
+    i = 0
+
+    flatchains = chains.flatten
+    sentence.each do |word|
+      word.suffix = "</font>"
+      word.prefix = "<font color=\"#{flatchains[i*3]}\">" 
+      i += 1
+    end
+
+    out += sentence.to_s + "<br />\n<br />\n"
+
+    srctext = Hash.new # Stores our original source text (eventually Sentences) for later
+
+    #Get our source text's chain id's
+    tids.each do |tid|
+      sent = exec "SELECT id FROM chains WHERE textid=$1", tid
+      sent.delete( sent[-1] )
+      srctext[tid] = sent.flatten
+    end
+
+    srcsent = Hash.new
+
+    #Now that we have both the source text chain id's and the quote's
+    #  we can flag text to be colored when it matches, in its entirety,
+    #  a chunk of the source text. We tag it with the color it needs.
+
+    res.length.times.each do |i|
+      tid = tids_index[i]
+      #print "TID: ", tid, "\n"
+      #print "src: ", srctext[tid], "\n", res[i], "\n\n"
+      if not srcsent.has_key? tid
+        srctext[tid] = chain_to_word srctext[tid]
+      end
+      res[i] = chain_to_word res[i]
+
+      #print "src: ", srctext[tid], "\n", "res: ", res[i], "\n"
+
+      ind = index_in srctext[tid], res[i] #Find the first occurance of this chain in this fragment & return index
+      len = res[i].length-1
+      #print "i: " + i.to_s + "\tind: " + ind.to_s + "\tlen: " + len.to_s + "\n\n"
+
+      if not srcsent.has_key? tid
+        srcsent[tid] = Sentence.new msg, ( srctext[tid] ) 
+      end
+
+      len.times do |j|
+        #if j+ind >= srctext[i].length 
+        #  break
+        #end
+        srcsent[tid][ind+j].prefix = "<font color=\"#{colors[i]}\">"
+        srcsent[tid][ind+j].suffix = "</font>" 
+      end  
+    end
+
+    srcsent.values.each do |src|
+      out += src.to_s + "<br />\n"
+    end
 
   out
+  end
 end
