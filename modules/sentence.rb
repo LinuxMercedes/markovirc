4be68257eba454547c6078fd40db1c62ddbf2844
Billy Rhoades <billyarhoades@gmail.com>
2014-09-04 19:09:23 -0500

    Cleaned up sentence class more, fixed +

--- modules/sentence.rb
+++ modules/sentence.rb
@@ -7,41 +7,13 @@ require_relative 'word.rb'
 This class allows easy manipulation of sentences
 without having to fumble with arrays and unshift all the time. This makes life
 so very much easier and the code look much cleaner in the end.
-
-Member variables:
-  @words
-    This variable houses our sentence in an array. Each member is a Word (class)
-    and contains their own wids / text.    
-  WORDS / WIDS
-    These are constants used only internally. They assist in determining the
-    argument passed to the initialize function to hand off to another function.
-
-Functions:
-  initialize( Array words )
-    Initialize is called on object creation and is passed an array of Integers,
-    a String, an Array of Strings, or an array of Words. If a string is passed
-    it intelligently splits and grabs the first array of strings handles it
-    like it was passed that. 
-
-    If passed an array of strings or wids, it converts them to Words and stores
-    them in @words. If passed nil, it just leaves as is with a blank array.
-
-  <<( word ):
-    This function simply pops a word onto the rightmost space of the sentence. It 
-    blindly converts the argument into a word if it isn't one already.
-
-  >>( word ):
-    This function simply pops a word onto the leftmost space of the sentence. It
-    blindly converts the argument into a word if it isn't one already.
-
-  Inheritied array functions: each, [], unshift, first, last
 """
 class Sentence 
   include Enumerable
   extend Forwardable
 
   def_delegators :@words, :each, :unshift, :first, :last, :[], :size, :length
-  attr_accessor :words, :msg
+  attr_accessor :words
 
   def initialize( words=nil )
     @words = [ ]
@@ -81,23 +53,6 @@ class Sentence
     end
 
     word
-  end
-
-  def join( joiner )
-    words = []
-    @words.each do |word|
-      words << word.text
-    end
-
-    words.join joiner
-  end
-  
-  def clean
-    @words.each do |word|
-      if not word.wid.is_a? Integer or word.wid < 1 or not word.text.is_a? String
-        @words.delete word
-      end
-    end 
   end
 
   def sever( sent )
@@ -158,6 +113,6 @@ class Sentence
   end
 
   def +( rhs )
-    self.join( " " ) + rhs
+    self.to_s + rhs.to_s
   end
 end
