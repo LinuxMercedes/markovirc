e89f779ebb6c21e2fb17c51fdf26edf8f25ef51f
Billy Rhoades <billyarhoades@gmail.com>
2014-10-06 19:27:38 -0500

    Removed old sever

--- utils.rb
+++ utils.rb
@@ -2,36 +2,6 @@ require 'settingslogic'
 require 'connection_pool'
 require 'thread_safe'
 
-# Intelligently split a sentence by punctuation, then split the individual spaces
-# so we get words.
-def sever( text )
-  # For now, quotes are stripped since handling them is tricky.
-  text.gsub! /"/, ""
-  sentences = text.scan /([^\.!:\?,]+)([\.!\?:,]+)?/ 
-  sentences.flatten!.compact!
-
-  last = 0 
-  while last != sentences.length
-    last = sentences.length
-
-    # Inspect for smashed urls
-    sentences.length.times do |i|
-      if sentences[i] =~ /^[\.!?:]+$/ and sentences.length > i+1 and sentences[i+1][0] !~ /[\s]/
-        sentences[i-1] = sentences[(i-1)..(i+1)].join ''
-        sentences.delete_at i
-        sentences.delete_at i
-        break 
-      end
-    end
-  end
-
-  sentences.map! { |x| x.split /\s+/ }
-  sentences.flatten!
-  sentences.delete_if { |x| x == "" }
-
-  return sentences
-end
-
 class Settings < Settingslogic
   if ARGV[0] == nil
     source "config.yml"
