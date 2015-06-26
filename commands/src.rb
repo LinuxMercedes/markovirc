d4db791609d074c71faac4d7cde467f238df14c2
Billy Rhoades <billyarhoades@gmail.com>
2015-06-26 17:35:40 -0500

    Restored !src from before the reset.

---
+++ commands/src.rb
@@ -0,0 +1,35 @@
+require 'cinch'
+require 'json'
+
+class Src 
+  include Cinch::Plugin
+  include Source 
+
+  match /(src|source)( .+)?/, method: :execute
+
+  def execute( m, name, args )
+    if args != nil
+      args = args.strip.to_i
+      if args == 0 or args == nil
+        args = 1
+      end
+    else
+      args = 1
+    end
+
+    #We're looking for 1 (most recent message) 2 (two messages ago) or higher.
+    if args > $bot.logs[m.channel].size or args < 1
+      return
+    end
+
+    sent = $bot.logs[m.channel][-1*args]
+    sent.chainids.delete []
+
+    chanid = m.getFirst_i "SELECT id FROM channels WHERE name=?", m.channel
+    m.getFirst "INSERT INTO quotes (channelid, chain) VALUES (?, ?)", [chanid,JSON.generate(sent.chainids)]
+    qid = m.getFirst_i "SELECT currval('quotes_id_seq')"
+
+    m.reply "#{m.bot.set.quoteurl}#{qid.to_s}", true
+  end
+end
+
